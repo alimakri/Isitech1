8142a45675c9cab04aef22d728046f47fa68392e
Ali MAKRI <github@makri.fr>
2023-02-27 12:46:19 +0100

    12:46

--- 4.sql
+++ 4.sql
@@ -14,8 +14,37 @@ CREATE DATABASE Formule1 ON  PRIMARY
 	)
 USE [Formule1]
 GO
-CREATE TABLE [dbo].[Pilote]
+CREATE TABLE [dbo].[Ecurie]
 (
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[Nom] [nvarchar](max) NOT NULL,
+	CONSTRAINT [PK_Ecurie] PRIMARY KEY CLUSTERED 
+	(
+	[Id] ASC
+	)
+) 
+GO
+CREATE TABLE [dbo].[EcurieGrandPrix](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[Ecurie] [bigint] NOT NULL,
+	[GrandPrix] [bigint] NOT NULL,
+	CONSTRAINT [PK_EcurieGrandPrix] PRIMARY KEY CLUSTERED 
+	(
+	[Id] ASC
+	)
+) 
+GO
+CREATE TABLE [dbo].[GrandPrix]
+(
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[Lieu] [nvarchar](max) NOT NULL,
+	CONSTRAINT [PK_GrandPrix] PRIMARY KEY CLUSTERED 
+	(
+	[Id] ASC
+	)
+)
+GO
+CREATE TABLE [dbo].[Pilote](
 	[Id] [bigint] IDENTITY(1,1) NOT NULL,
 	[Nom] [nvarchar](max) NOT NULL,
 	[Ecurie] [bigint] NULL,
@@ -29,4 +58,20 @@ CREATE TABLE [dbo].[Pilote]
 GO
 ALTER TABLE [dbo].[Pilote] ADD  CONSTRAINT [DF_Pilote_Actif]  DEFAULT ((0)) FOR [Actif]
 GO
+-- Clés étrangères 
+--EcurieGrandPrix
+ALTER TABLE [dbo].[EcurieGrandPrix]  WITH CHECK ADD  CONSTRAINT [FK_EcurieGrandPrix_Ecurie] FOREIGN KEY([Ecurie]) REFERENCES [dbo].[Ecurie] ([Id])
+ALTER TABLE [dbo].[EcurieGrandPrix] CHECK CONSTRAINT [FK_EcurieGrandPrix_Ecurie]
+
+ALTER TABLE [dbo].[EcurieGrandPrix]  WITH CHECK ADD  CONSTRAINT [FK_EcurieGrandPrix_GrandPrix] FOREIGN KEY([GrandPrix]) REFERENCES [dbo].[GrandPrix] ([Id])
+ALTER TABLE [dbo].[EcurieGrandPrix] CHECK CONSTRAINT [FK_EcurieGrandPrix_GrandPrix]
+
+-- Pilote
+ALTER TABLE [dbo].[Pilote]  WITH CHECK ADD  CONSTRAINT [FK_Pilote_Ecurie] FOREIGN KEY([Ecurie]) REFERENCES [dbo].[Ecurie] ([Id])
+ALTER TABLE [dbo].[Pilote] CHECK CONSTRAINT [FK_Pilote_Ecurie]
+
+
+
+
+
 
