4b7f02fb9ebf6a50cf0ac8d04cc9adc8dc62308a
Ali MAKRI <github@makri.fr>
2023-02-28 17:12:25 +0100

    17:12

--- Requetes/10.sql
+++ Requetes/10.sql
@@ -1,17 +1,21 @@
 use AdventureWorks2017
 GO
-ALTER PROC ListeProduits(@couleur nvarchar(50)=null, @seuil int=null)
+ALTER PROC ListeProduits(@couleur nvarchar(50)=null, @seuil int=null, @type char(2)=null)
 AS
 select 
 	ProductID, ProductNumber, Color, ListPrice 
 from 
 	Production.Product
 where 
-	Color=@couleur 
+	(@couleur is null or Color=@couleur) 
 	and 
-	ListPrice > @seuil
+	(@seuil is null or ListPrice > @seuil)
+	and
+	(@type is null or ProductNumber like @type + '%')
 GO
 
-Exec ListeProduits 'Red', 1000
+Exec ListeProduits 'Red', 1000, 'BK'
 Exec ListeProduits 'Red'
 Exec ListeProduits
+Exec ListeProduits @couleur='Red', @type='BK'
+Exec ListeProduits @seuil=2000, @type='BK'
