4c9beb7e47785faa54bf0712363a72246bbe1c6a
Ali MAKRI <github@makri.fr>
2023-03-01 17:21:52 +0100

    17:21

--- Requetes/15.sql
+++ Requetes/15.sql
@@ -1,9 +1,20 @@
+use vente
+GO
+ALTER PROC EditionProduit(@data xml)
+AS
+	-- Récupération XML
+	SELECT
+		N.value('@op[1]', 'nvarchar(MAX)') AS op,
+		N.value('@id[1]', 'uniqueidentifier') AS id,
+		N.value('libelle[1]', 'nvarchar(MAX)') AS libelle,
+		N.value('prix[1]', 'decimal(18,2)') AS prix
+	into #t
+	FROM
+		@data.nodes('/produits/produit') AS T(N)
 
-
-
-
-select * from Produit
-
+	-- Delete
+	delete Produit where id in (select id from #t where op='delete')
+GO
 Exec EditionProduit '
 					<produits>
 						<produit op="delete" id="CB9A7ECA-A7EA-4050-B243-01AE1EFA74CA"/>
