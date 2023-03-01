59a545c903194c708fc14fef631c98a9606b5f87
Ali MAKRI <github@makri.fr>
2023-03-01 09:33:06 +0100

    09:32

--- Requetes/11.sql
+++ Requetes/11.sql
@@ -40,3 +40,37 @@ where
 	(@type is null or ProductNumber like @type + '%')
 GO
 Exec ListeProduitsXML '.....'
+
+-- *****************************************************************************
+-- Solution
+-- *****************************************************************************
+CREATE PROC ListeProduitsXML(@data xml)
+AS
+DECLARE @couleur nvarchar(50)
+DECLARE @seuil nvarchar(50)
+DECLARE @type nvarchar(50)
+
+-- ****
+
+-- ****
+
+IF @couleur = 'Rouge'
+  BEGIN
+  SET @couleur='red'
+  END
+ELSE IF @couleur = 'Noir'
+	SET @couleur='black' 
+
+select 
+	ProductID, ProductNumber, Color, ListPrice 
+from 
+	Production.Product
+where 
+	(@couleur is null or Color=@couleur) 
+	and 
+	(@seuil is null or ListPrice > @seuil)
+	and
+	(@type is null or ProductNumber like @type + '%')
+GO
+Exec ListeProduitsXML '<params couleur="Red" seuil="1000" type="BK"></params>'
+--Exec ListeProduitsXML '<data t="BK"><c>Red</c><s>1000</s></data>'
