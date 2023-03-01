c82407a000aa8ac613f6f812a7f079d13a6fa5e5
Ali MAKRI <github@makri.fr>
2023-03-01 15:25:06 +0100

    15:25

--- Requetes/14.sql
+++ Requetes/14.sql
@@ -9,10 +9,10 @@ Exec AjoutProduit 'Jogging', 35.50
 Exec AjoutProduit 'Polo d''action', 58.80
 Exec AjoutCommande '
 					<commandes>
-						<commande id="80850DA3-1C2D-4362-845D-9D02097BBB08">
+						<commande id="4F8B4BCF-2507-4F7B-8405-CA41FDC74911">
 							<qte>2</qte>
 						</commande>
-						<commande id="D62F13A5-E396-4BC8-BF2C-B00CE5250CA4">
+						<commande id="F189CEE0-4901-4F76-B8E0-C4EFEF6E2BC9">
 							<qte>3</qte>
 						</commande>
 					</commandes>'
@@ -21,15 +21,32 @@ CREATE PROC AjoutProduit(@libelle nvarchar(MAX), @prix money)
 AS
 	Insert produit (Libelle, Prix) values(@libelle, @prix)
 GO
-CREATE PROC AjoutCommande(@data xml)
+ALTER PROC AjoutCommande(@data xml)
 AS
 	SELECT
 		N.value('@id[1]', 'uniqueidentifier') AS id,
 		N.value('qte[1]', 'INT') AS Qte
+	into #t
 	FROM
 		@data.nodes('/commandes/commande') AS T(N)
 
+	DECLARE @id uniqueidentifier = newid()
 	
+	INSERT Commande (Id, Numero, DateCommande)
+	values(@id, convert(varchar(8),getdate(),112), getdate())
+
+	 Insert CommandeDetails (Id, Commande, Produit, Qte, Prix) 
+	 select Newid(), @id, #t.id, #t.Qte,p.Prix  from #t 
+	 inner join Produit p on #t.id = p.Id
+
 GO
 
 select * from Produit order by Libelle
+select * from Commande c inner join CommandeDetails d on d.Commande = c.Id
+where d.Produit in ('4F8B4BCF-2507-4F7B-8405-CA41FDC74911', 'F189CEE0-4901-4F76-B8E0-C4EFEF6E2BC9')
+
+select * from commande  where id='2F9B7FC3-6B3A-40B6-A5C2-145F127CCA19'
+select * from CommandeDetails where Commande='2F9B7FC3-6B3A-40B6-A5C2-145F127CCA19'
+
+-- 21 394
+delete commande where Numero like '2023%'
