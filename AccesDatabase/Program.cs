a4f951649ba4a48685aec7d111388a98c9774100
Ali MAKRI <github@makri.fr>
2023-03-01 13:06:44 +0100

    13:06

--- AccesDatabase/Program.cs
+++ AccesDatabase/Program.cs
@@ -12,6 +12,17 @@ namespace AccesDatabase
     {
         static void Main(string[] args)
         {
+            //ClassesBases();
+            EntityFramework();
+            Console.Read();
+        }
+
+        private static void EntityFramework()
+        {
+        }
+
+        private static void ClassesBases()
+        {
             var cnx = new SqlConnection();
             cnx.ConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=AdventureWorks2017;Integrated Security=True";
             cnx.Open();
@@ -22,24 +33,30 @@ namespace AccesDatabase
             cmd.CommandText = "select ProductID, Name from Production.Product";
 
             // Mode connecté
-            //SqlDataReader rd = cmd.ExecuteReader();
-            //while (rd.Read())
-            //{
-            //    Console.WriteLine("{0} - {1}", rd["ProductID"], rd["Name"]);
-            //}
-            //rd.Close();
+            SqlDataReader rd = cmd.ExecuteReader();
+            while (rd.Read())
+            {
+                Console.WriteLine("{0} - {1}", rd["ProductID"], rd["Name"]);
+            }
+            rd.Close();
+
+            cmd.CommandText = "update Production.Product set Name=Name+'_YYY' where ProductId=1";
+            cmd.ExecuteNonQuery();
 
             // Mode déconnecté
-            var da = new SqlDataAdapter();
-            da.SelectCommand = cmd;
-            var ds = new DataSet();
-            da.Fill(ds);
+            //var da = new SqlDataAdapter();
+            //var cb = new SqlCommandBuilder(da);
+            //da.SelectCommand = cmd;
+            //var ds = new DataSet();
+            //da.Fill(ds);
 
-            foreach (DataRow row in ds.Tables[0].Rows)
-            {
-                Console.WriteLine("{0} - {1}", row["ProductID"], row["Name"]);
-            }
-            Console.Read();
+            //foreach (DataRow row in ds.Tables[0].Rows)
+            //{
+            //    Console.WriteLine("{0} - {1}", row["ProductID"], row["Name"]);
+            //}
+
+            //ds.Tables[0].Rows[0]["Name"] += "_XXX";
+            //da.Update(ds);
         }
     }
 }
---
+++ LinqDemo/LinqDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new AdvContext();
+            var p = context.Products.FirstOrDefault();
+            p.Name = "Adjustable Race";
+            context.SaveChanges();
+
+            var velos = context.Products.Where(x => x.ProductSubcategory.ProductCategory.Name == "Clothing");
+            foreach(var velo in velos)
+            {
+                Console.WriteLine(velo.Name);
+            }
+
+            var listeEntier = new int[] { 1, 52, 69, 54, 88, 23 };
+
+            var reponse = listeEntier.Where(x => x % 2 == 1);
+            foreach (var i in reponse)
+            {
+                Console.WriteLine(i);
+            }
+            Console.Read();
+        }
+    }
+}
