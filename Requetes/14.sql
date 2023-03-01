6d9f8bebebf6ed844c618b5e896a7358f6acb267
Ali MAKRI <github@makri.fr>
2023-03-01 14:54:41 +0100

    14:54

--- Requetes/14.sql
+++ Requetes/14.sql
@@ -5,15 +5,31 @@
 -- Ajouter Commande : 2 jogging et 3 polos
 -- ****************************************************
 use Vente;
-Exec AjoutProduit 'Jogging' 35.50
-Exec AjoutProduit 'Polo d''action' 58.80
+Exec AjoutProduit 'Jogging', 35.50
+Exec AjoutProduit 'Polo d''action', 58.80
 Exec AjoutCommande '
 					<commandes>
-						<commande id="1">
+						<commande id="80850DA3-1C2D-4362-845D-9D02097BBB08">
 							<qte>2</qte>
 						</commande>
-						<commande id="2">
+						<commande id="D62F13A5-E396-4BC8-BF2C-B00CE5250CA4">
 							<qte>3</qte>
 						</commande>
 					</commandes>'
+GO
+CREATE PROC AjoutProduit(@libelle nvarchar(MAX), @prix money)
+AS
+	Insert produit (Libelle, Prix) values(@libelle, @prix)
+GO
+CREATE PROC AjoutCommande(@data xml)
+AS
+	SELECT
+		N.value('@id[1]', 'uniqueidentifier') AS id,
+		N.value('qte[1]', 'INT') AS Qte
+	FROM
+		@data.nodes('/commandes/commande') AS T(N)
 
+	
+GO
+
+select * from Produit order by Libelle
